f2dd59690431bf19a48195f2e5dc261e4ea81a0d
Alphy Gacheru <alphygacheru@gmail.com>
2022-07-27 14:27:32 +0300

    Send data to the api on add score form submit

--- modules/addScore.js
+++ modules/addScore.js
@@ -1,3 +1,23 @@
-const addScore = document.querySelector('#add-score')
-const userName = document.querySelector('#name')
-const userScore = document.querySelector('#score')
+const addScore = document.querySelector('#add-score');
+const userName = document.querySelector('#name');
+const userScore = document.querySelector('#score');
+
+addScore.addEventListener('submit', (e) => {
+  e.preventDefault();
+  fetch(
+    'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/CWfQ94AJNRuIhIWKDLIg/scores',
+    {
+      method: 'POST',
+      body: JSON.stringify({
+        user: userName.value,
+        score: userScore.value,
+      }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    },
+  );
+
+  userName.value = '';
+  userScore.value = '';
+});
